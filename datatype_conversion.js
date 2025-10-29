a3a958b0a57861f02a76c0d03a122f1f869633be
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2025-10-29 08:19:02 +0000

    addded datatype conversine

---
+++ datatype_conversion.js
@@ -0,0 +1,12 @@
+let score = "undefine";
+
+console.log(typeof score);
+let value = Number(score);
+console.log(value);
+
+
+let isLoggedIn = 0;
+let booleanValue = Boolean(isLoggedIn);
+console.log(booleanValue);
+
+
