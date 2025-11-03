e6373c06dddbcc4d2db08e5c22bb058e08b01239
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2025-11-03 09:27:38 +0000

    js Number and maths changes are added

---
+++ nums_maths.js
@@ -0,0 +1,20 @@
+let number = 100;
+console.log(number);
+
+let balance = new Number(500);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+let otherNumber = 168.14;
+console.log(otherNumber.toPrecision(3));
+
+
+/**********************Math**************************/
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(168.9));
+console.log(Math.ceil(5.3));
+console.log(Math.floor(2.7));
+console.log(Math.min(5,6,9,2,0,7));
+console.log(Math.max(6,8,3,7,2,100));
