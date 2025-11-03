7be64bd9b3c31d7abde838eba76856a1af04d397
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2025-11-03 07:18:18 +0000

    heap and stack mem code added

---
+++ memory.js
@@ -0,0 +1,19 @@
+let youtube = "komalrajole";
+
+let anotherYoutube = youtube;
+
+anotherYoutube = "chaiaurcode";
+
+
+console.log(anotherYoutube);
+console.log(youtube); 
+
+let oneMem = {
+    email : "komal@gmail.con",
+    pass  : "123456"
+}
+
+let twoMem = oneMem;
+
+console.log(oneMem.email);
+console.log(twoMem.email);
