a57d392fc98ab0b1d8f9ad4652109622411e2b2a
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2025-11-10 10:50:49 +0000

    added scope js changes

---
+++ 03_scope.js
@@ -0,0 +1,34 @@
+let a= 10
+const b = 20
+var c = 30
+
+if(true){
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+
+console.log(a)
+
+function one(){
+    userName = "KOmal"
+    function two(){
+        website = " youtube"
+        console.log("Username is", userName)
+        console.log ( userName + website)
+    }
+    // console.log("website is", website)
+    two()
+}
+one()
+console.log("*******************************")
+if(true){
+    const usersName = "Ganesh";
+    if(usersName === "Ganesh"){
+         const websites = "Google";
+        console.log( usersName + websites)
+    }
+    // console.log("website name is", websites)
+       
+}
+// console.log("username is",usersName)
