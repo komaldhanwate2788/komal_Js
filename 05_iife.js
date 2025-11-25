0914816f0363313fc9f06f075cac88d7f1ac480a
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2025-11-25 08:52:33 +0000

    IIFE code added

---
+++ 05_iife.js
@@ -0,0 +1,12 @@
+function chai(){
+    console.log("DB Connected");
+}
+chai();
+
+(function aurchai(){
+    console.log("DB COnnected");
+})();
+
+( (username) => {
+    console.log(`${username} is an Engineer`);
+}) ("komal")
