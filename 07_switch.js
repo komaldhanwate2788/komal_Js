48618c2282caa2f69dc535d85a50c8a7ea25be9e
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2025-11-28 09:29:41 +0000

    another controle flow added

---
+++ 07_switch.js
@@ -0,0 +1,31 @@
+const month = 3
+// const month = "march"
+switch(month){
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("MArch");
+        break;
+    default:
+        console.log("Not month")
+        break;
+}
+
+// switch(month){
+//     case "jan":
+//         console.log("Jan");
+//         break;
+//     case "feb":
+//         console.log("Feb");
+//         break;
+//     case "march":
+//         console.log("MArch");
+//         break;
+//     default:
+//         console.log("Not month")
+//         break;
+// }
---
+++ 08_tuthy.js
@@ -0,0 +1,17 @@
+// const userEmail = " "
+
+// if(userEmail){
+//     console.log("GOt useremail");
+// }else{
+//     console.log("Do not have user email")
+// }
+
+// const userEmail1 = []
+// if(userEmail1.length == 0){
+//     console.log("Array is empty")
+// }
+
+const userEmail = {}
+if(Object.keys(userEmail).length == 0){
+    console.log("Object is empty")
+}
