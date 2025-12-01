417307af7a41b55a5ba47ed783ede14e383f528d
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2025-12-01 06:23:14 +0000

    added while and do while llop changes by komalk

---
+++ 10_loopwhile.js
@@ -0,0 +1,18 @@
+// let i =0
+// while(i <= 10){
+//     console.log(`Value of i is ${i}`)
+//     i = i +2
+// }
+
+// const myArray = ["flash","superman","batman","Ironman","Thor","perman"]
+// let index = 0;
+// while(index < myArray.length){
+//     console.log(`My heros is ${myArray[index]}`)
+//         index++
+// }
+
+let score = 100
+do{
+    console.log(`Score is ${score}`)
+    score++
+}while(score < 10)
