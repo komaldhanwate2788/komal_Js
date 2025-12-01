240237b3f9d55554007031c3fff73f77c4286ab4
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2025-12-01 05:55:43 +0000

    for loop changes added by komal rajole

---
+++ 09_for.js
@@ -0,0 +1,46 @@
+// for(let index = 0; index <= 10; index++){
+//     const elemnet = index;
+//     console.log(elemnet)
+// }
+
+// for(let i = 0; i <= 10;i++){
+//     const elemnet = i;
+//     console.log(elemnet)
+// }
+//  console.log(elemnet)
+
+// for(let i =0;i <= 10; i++){
+//     console.log(`Outer loop of ${i}`)
+//     for(let j=0;j<=10;j++){
+//         console.log(`Inner loop of ${j} and ${i}`)
+//     }
+// }
+
+// for(let i=0;i < 5;i++){
+//     console.log("*".repeat(i))
+    
+// }
+
+// for(i=0;i<10;i++){
+//     const element =i
+//     if(element == 5){
+//         console.log("5 is best number")
+//     }
+//     console.log(i)
+// }
+
+// const myArray = ["flash","batman","superman","perman","Ironman","thor"]
+// for(let i =0;i< myArray.length;i++)
+// {
+//     console.log(myArray[i])
+// }
+
+for(let i =0;i<=20;i++){
+    const element = i;
+    if(element == 5){
+        console.log(`Detected element is ${i}`)
+        // continue
+        break
+    }
+    console.log(i)
+}
