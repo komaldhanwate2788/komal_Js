ce885b366c19f7a4787e37685836942b799648ff
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2026-01-01 07:58:58 +0000

    changes file

--- 17_map.js
+++ 17_map.js
@@ -1,3 +1,7 @@
 const myNum = [1,2,3]
 
-const newNumbers = myNum.reduce(function((acc,currval) => ))
+const newNumbers = myNum.reduce(function(acc,currval)  {
+    console.log(`acc: ${acc} and currval:${currval}`)
+    return acc + currval
+},0 )
+console.log(newNumbers)
