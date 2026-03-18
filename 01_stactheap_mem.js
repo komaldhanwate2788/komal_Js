870677933d67fc991b650b7ca09a28b46639a508
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2026-03-18 11:38:16 +0000

    strinf js code addded

---
+++ 01_stactheap_mem.js
@@ -0,0 +1,14 @@
+let myYoutubeName = "komalRajole"
+let anotherName = myYoutubeName
+anotherName = "CrazzyKomal"
+console.log(anotherName)
+
+
+let userOne = {
+    email:"komal@microsoft.com",
+    upi:"koma@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "komal@goggle.com"
+console.log(userTwo)
---
+++ 02_string.js
@@ -0,0 +1,19 @@
+let myName = "komalKR"
+let myrepoCount = 500
+
+console.log(`Hello my name is ${myName} and my repocnt is ${myrepoCount}`)
+
+let myAdd = new String("KomalGaneshRajoleDeveloper")
+let position = "Developer"
+console.log(myAdd.toUpperCase())
+console.log(myAdd.toLowerCase())
+console.log(myAdd.length)
+console.log(myAdd.charAt(5))
+console.log(myAdd.indexOf('k'))
+console.log(myAdd.concat(position))
+// console.log(result)
+// console.log(myAdd.substring(5,9))
+// console.log(myAdd.repeat(5))
+console.log(myAdd.replace('Developer','Senior Developer'))
+console.log(myAdd.split(''))
+console.log(myAdd.slice(-1,9))
