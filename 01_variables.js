fdb5174ab2e6a4091dddae72205f789982fae103
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2026-03-18 08:55:19 +0000

    added new code

---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12456
+let name = "komal rajole"
+var address = "nashik"
+// myCity - "pathardiphata"
+
+// accountId = 25689
+console.log(accountId);
+console.table([accountId,name,address])
