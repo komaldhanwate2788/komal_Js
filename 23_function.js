4e474d2ddf2cbb95224791a5e19ce952c3d50cf4
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2026-03-21 13:31:53 +0000

    function code in js added

---
+++ 23_function.js
@@ -0,0 +1,37 @@
+// console.log("k")
+// console.log("k")
+// console.log("k")
+// console.log("k")
+// console.log("k")
+
+function myName(){
+    console.log("k")
+console.log("k")
+console.log("k")
+console.log("k")
+console.log("k")
+}
+// myName()
+
+// function addNum(num1,num2){
+//   console.log(num1 + num2)
+// }
+// addNum(2,3)
+
+function addNum(num1,num2){
+  let res = num1 + num2
+  return res
+}
+
+const res = addNum(2,3)
+
+console.log("REsult :", res)
+
+function myFullname(name = "abc"){
+    if(!undefined){
+        console.log("please enter username")
+        return
+    }
+ return `Hello my name is ${name}`
+}
+console.log(myFullname())
