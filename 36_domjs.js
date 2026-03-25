888be9e187e7a34180f9f30ee02edc40da8cc7f5
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2026-03-25 08:46:35 +0000

    dom code

---
+++ 36_domjs.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>This My first DOM</title>
+        <meta charSet="UTF-8">
+    </head>
+    <body>
+        <div class="bg-black">
+            <h1 id="title" class="heading">Hello..... Welcome in my first DOM</h1>
+            <p>Document Object Module in JS</p>
+        </div>
+        
+    </body>
+</html>
