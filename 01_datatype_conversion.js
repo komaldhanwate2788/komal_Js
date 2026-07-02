282dde843e52fb762d3ebb2d61338230e6b720bc
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2026-07-02 09:05:16 +0000

    changes added

--- 01_datatype_conversion.js
+++ 01_datatype_conversion.js
@@ -6,3 +6,15 @@ console.log(typeof(score))
 let valueInnumber = Number(score)
 console.log(typeof valueInnumber)
 console.log(valueInnumber)
+
+
+
+// let num = 23
+// console.log(num)
+// let num_Con = String(num)
+// console.log(num_Con)
+// console.log(typeof num_Con)
+
+let value  = 0
+let newVal = Boolean(value)
+console.log(typeof newVal)
