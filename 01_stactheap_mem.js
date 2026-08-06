0f1d9732a6cd20357ce7f28d0bb4b0536d52a342
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2026-08-06 08:10:09 +0000

    new code added

--- 01_stactheap_mem.js
+++ 01_stactheap_mem.js
@@ -1,14 +1,25 @@
-let myYoutubeName = "komalRajole"
-let anotherName = myYoutubeName
-anotherName = "CrazzyKomal"
-console.log(anotherName)
+// let myYoutubeName = "komalRajole"
+// let anotherName = myYoutubeName
+// anotherName = "CrazzyKomal"
+// console.log(anotherName)
 
 
-let userOne = {
-    email:"komal@microsoft.com",
-    upi:"koma@ybl"
+// let userOne = {
+//     email:"komal@microsoft.com",
+//     upi:"koma@ybl"
+// }
+
+// let userTwo = userOne
+// userTwo.email = "komal@goggle.com"
+// console.log(userTwo)
+
+
+let stude1 = {
+    email:"koml@gmail.com",
+    pass : 123
 }
+console.log(stude1.email)
+let stude2 = stude1
+stude2.email = "komal@yahoo.com"
 
-let userTwo = userOne
-userTwo.email = "komal@goggle.com"
-console.log(userTwo)
+console.log(stude2.email)
