b97e4f2616f99dab30b86097ef1585137e8cc06c
komaldhanwate2788 <komaldhanwate2788@gmail.com>
2026-08-06 09:00:08 +0000

    new code

---
+++ 41_jsroadmap.js
@@ -0,0 +1,43 @@
+const name = "komal"
+const repocount = 50 
+
+console.log(`My name is ${name} and my repo count is ${repocount}`)
+
+const gname = new String("KOmalrajole")
+console.log(typeof(gname))
+
+console.log(gname[1])
+console.log(gname.__proto__)
+
+console.log(gname.length)
+console.log(gname.toUpperCase())
+console.log(gname.padEnd())
+console.log(gname.charAt(5))
+console.log(gname.substring(2,7))
+
+
+const newStr = gname.substring(4,9)
+console.log(newStr)
+
+const newOne = gname.slice(-1,5)
+console.log(newOne)
+
+const newStr1 = " hefh rjeruiortort tyr"
+console.log(newStr1.length)
+const newStr2 = newStr1.trim()
+console.log(newStr2.length)
+console.log(newStr1.replace('hefh','okok'))
+console.log(newStr1.includes('iort'))
+const url ="https://www.youtube.com/watch?v=sscX432bMZo&t=5461s"
+console.log(url.replace('/','-'))
+
+const score = 500
+console.log(score)
+
+const balance = new Number(4000)
+console.log(typeof(balance))
+console.log(balance.toString().length)
+console.log(typeof(balance))
+
+const otherNum = 3.1454854
+console.log(otherNum.toFixed(2))
